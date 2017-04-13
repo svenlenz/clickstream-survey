988d7e49317a6cc4bf47aa1adb0395f4f2bf73ac
Sven Lenz <sle@finnova.ch>
2017-04-13 09:31:53 +0200

    removed unused factory

--- survey/app/app/common/common.module.js
+++ survey/app/app/common/common.module.js
@@ -1,23 +1,5 @@
 angular.module('app.common', [])
 
-    .factory('GetJSonMI',['$http', function ($http){
-    var allData= null;
-    var getallData = function(callBack){
-
-        $http.get('/poc-backend/MI?').
-            then(function(respond){
-                allData = angular.fromJson(respond.data);
-                console.log(allData);
-                callBack(allData);
-            }, function(respond) {
-            })
-    }
-
-    return {
-        getAllLocations: getallData
-    }
-
-}])
 .factory('surveyFactory',['$http', function ($http){
 
     var returned= null;
